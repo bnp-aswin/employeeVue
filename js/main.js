7d288e182a0cd9df96fd2c9214d055b3501fe2d9
aswinblix <aswin.blix@bitsnpixs.com>
2021-06-21 19:38:38 +0530

    add vue directive and add employees and view employees

--- js/main.js
+++ js/main.js
@@ -12,4 +12,54 @@ let employeeDetails = new Vue({
         disableInput: false,
         selectedRow: null,
     },
+    methods: {
+          clearField() {
+            this.firstName = "";
+            this.lastName = "";
+            this.employeeCode = "";
+            this.dateofBirth = "";
+            this.designation = "";
+            this.employee = {};
+        },
+            addEmployee() {
+            this.employee = {
+                first_name: this.firstName,
+                last_name: this.lastName,
+                employee_code: this.employeeCode,
+                dateof_birth: this.dateofBirth,
+                designation: this.designation,
+            };
+            this.employees.push(this.employee);
+            employeeDetails.clearField();
+        },
+         checkEmployee() {
+            console.log(!this.firstName);
+            if (
+                !this.firstName ||
+                !this.lastName ||
+                !this.employeeCode ||
+                !this.dateofBirth ||
+                !this.designation
+            ) {
+                alert("Please fill all fields");
+                return;
+            }
+            if (this.employees.length !== 0) {
+                let employeesId = [];
+                for (let index = 0; index <= this.employees.length - 1;index++) {
+                    employeesId.push(this.employees[index].employee_code);
+                }
+                if (employeesId.includes(this.employeeCode, 0)) {
+                    alert("Emplopyee Id already Exist");
+                    employeeDetails.clearField();
+                } else {
+                    employeeDetails.addEmployee();
+                    employeeDetails.clearField();
+                }
+            } else {
+                employeeDetails.addEmployee();
+                employeeDetails.clearField();
+            }
+        },
+    },
 });
